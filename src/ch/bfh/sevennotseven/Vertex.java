5ac72ae909dffb41162c5ad970840c4e379a8b58
id101010 <aaron@duckpond.ch>
2016-06-17 00:50:39 +0200

    Added javadoc to vertex.java

--- src/ch/bfh/sevennotseven/Vertex.java
+++ src/ch/bfh/sevennotseven/Vertex.java
@@ -4,36 +4,80 @@ import java.awt.Point;
 
 public class Vertex {
 	
+	/* Class Members */
 	private int dist;
 	private Point pos;
 	private Vertex prev;
 	
+	/**
+	 * Costructor
+	 * 
+	 * @author aaron
+	 * @param int dist
+	 * @param Point pos
+	 */
 	public Vertex(int dist, Point pos) {
 		this.dist = dist;
 		this.pos = pos;
 		this.prev = new Vertex(0, new Point(0,0));
 	}
-	
+
+	/**
+	 * Set the distance
+	 * 
+	 * @author aaron
+	 * @param int dist
+	 */
 	public void setDist(int dist){
 		this.dist = dist;
 	}
 	
+	/**
+	 * Set current position
+	 * 
+	 * @author aaron
+	 * @param Point pos
+	 */
 	public void setPos(Point pos){
 		this.pos = pos;
 	}
 	
+	/**
+	 * Set previous vertex
+	 * 
+	 * @author aaron
+	 * @param Vertex prev
+	 */
 	public void setPrev(Vertex prev){
 		this.prev = prev;		
 	}
 	
+	/**
+	 * Get the distance 
+	 * 
+	 * @author aaron
+	 * @return int dist
+	 */
 	public int getDist(){
 		return this.dist;
 	}
 	
+	/** 
+	 * Get current position
+	 * 
+	 * @author aaron
+	 * @return Point pos
+	 */
 	public Point getPos(){
 		return this.pos;
 	}
 	
+	/** 
+	 * Get previous vertex
+	 * 
+	 * @author aaron
+	 * @return Vertex prev
+	 */
 	public Vertex getPrev(){
 		return this.prev;
 	}
